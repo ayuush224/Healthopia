095756fc97646a48a426da56488c6666290ea218
Ayush22 <ayushkumar05827@gmail.com>
2026-04-20 12:24:07 +0530

    Implemented Password salting and fixed some bugs
    Fixed the bug of communities members joined static values to dynamic, now it shows the correct no of members joined in community and also storing the values of noOfActiveMembers

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,6 +4,7 @@ const asyncHandler = require('../utils/asyncHandler');
 const { AppError } = require('../utils/errors');
 const { attachAuthCookie, clearAuthCookie } = require('../utils/authTokens');
 const { ensureString, normalizeUsername } = require('../utils/validation');
+const bcrypt = require("bcrypt");
 
 function serializeAuthUser(user) {
   return {
@@ -15,10 +16,16 @@ function serializeAuthUser(user) {
   };
 }
 
+async function getSaltedPassword(password){
+  const salt = await bcrypt.genSalt(12);
+  const hash = await bcrypt.hash(password, salt);
+  return hash;
+}
+
 const register = asyncHandler(async (req, res) => {
   const username = normalizeUsername(req.body.username);
   const email = ensureString(req.body.email, 'Email', { min: 5, max: 254 });
-  const password = ensureString(req.body.password, 'Password', { min: 8, max: 128 });
+  let password = ensureString(req.body.password, 'Password', { min: 6, max: 128 });
 
   const existingUser = await User.findOne({
     $or: [
@@ -30,6 +37,8 @@ const register = asyncHandler(async (req, res) => {
   if (existingUser) {
     throw new AppError('An account with that email or username already exists.', 409);
   }
+
+  password = await getSaltedPassword(password);
 
   const user = await User.create({
     name: username,
@@ -58,11 +67,16 @@ const register = asyncHandler(async (req, res) => {
 
 const login = asyncHandler(async (req, res) => {
   const email = ensureString(req.body.email, 'Email', { min: 5, max: 254 });
-  const password = ensureString(req.body.password, 'Password', { min: 6, max: 128 });
+  let password = ensureString(req.body.password, 'Password', { min: 6, max: 128 });
 
-  const user = await User.findOne({ email, password });
+  const user = await User.findOne({ email });
   if (!user) {
-    throw new AppError('No account was found for that email and password.', 401);
+    throw new AppError('No account was found for that email.', 401);
+  }
+  
+  let isMatch = bcrypt.compare(password, user.password);
+  if(!isMatch){
+    throw new AppError("Please Enter Correct Paswword");
   }
 
   attachAuthCookie(res, user._id.toString());
